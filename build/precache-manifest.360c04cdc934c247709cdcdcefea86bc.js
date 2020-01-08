4755384aabb32533046a38f8d8c54e33bb4d5870
SHergibo <sachau888@hotmail.com>
2020-01-08 14:25:55 +0100

    ajout build

---
+++ build/precache-manifest.360c04cdc934c247709cdcdcefea86bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6cc5e2a7da0b3fcd200c7e750cde061",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9e810a00878d796d858f",
+    "url": "/static/css/main.c1fb8eb2.chunk.css"
+  },
+  {
+    "revision": "4fed3e2b7cc756396a10",
+    "url": "/static/js/2.bce8c84b.chunk.js"
+  },
+  {
+    "revision": "c88dcf562e200fef59fdb4fa4867d5d1",
+    "url": "/static/js/2.bce8c84b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9e810a00878d796d858f",
+    "url": "/static/js/main.9fe34f39.chunk.js"
+  },
+  {
+    "revision": "7408695029b50dbeae77",
+    "url": "/static/js/runtime-main.730a111b.js"
+  }
+]);
