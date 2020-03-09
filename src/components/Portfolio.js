a088aee5f1109326360076f05c34ef0742b5a087
SHergibo <sachau888@hotmail.com>
2020-03-09 16:33:33 +0100

    travail sur la partie portfolio

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,9 +1,65 @@
-import React from 'react';
+import React, { useState } from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlus, faLink } from '@fortawesome/free-solid-svg-icons';
+import Modal from "./Modal";
 // import PropTypes from 'prop-types';
 
 function Portfolio() {
+  const [displayForm, setDisplayForm] = useState(false);
+  const [arrayProject, setArrayProject] = useState([{
+    _id: "0",
+    projectName : "Site CV",
+    imgProject : "site_cv_portfolio.png",
+    altImgProject : "Site CV Hergibo Sacha",
+    descriptionProject : "Lorem Ipsum",
+    urlProject : "#"
+  },
+  {
+    _id: "1",
+    projectName : "Site CV",
+    imgProject : "site_cv_portfolio.png",
+    altImgProject : "Site CV Hergibo Sacha",
+    descriptionProject : "Lorem Ipsum",
+    urlProject : "#"
+  }]);
+  let body = document.getElementsByTagName("body")[0];
+
+  const displayModal = () => {
+    body.setAttribute('style', 'overflow : hidden;');
+    setDisplayForm(true);
+  }
+
+  const closeModal = () => {
+    body.removeAttribute('style');
+    setDisplayForm(false);
+  } 
+
+  let projectPortfolio = arrayProject.map((item) => {
+    return <div className="project">     
+    <img src={`./${item.imgProject}`} alt={item.altImgProject} />
+    <div className="project-name">
+      {item.projectName}
+    </div>
+    <div className="project-more">
+      <div>
+        <span onClick={() => displayModal()}><FontAwesomeIcon icon={faPlus} /></span>
+        <span>
+          <a href={item.urlProject}><FontAwesomeIcon icon={faLink} /></a>
+        </span>
+      </div>
+    </div>
+  </div>
+  });
+
+  const div = <div className="info-project-modal">
+                <div className="img-project">
+                  <img src="./site_cv_portfolio.png" alt="Site CV Hergibo Sacha" />
+                </div>
+                <div className="info-project">
+                  <h2>Site CV</h2>
+                  <p>Lorem Ipsum</p>
+                </div>
+              </div>;
   
   return (
     <div className="portfolio-container">
@@ -12,27 +68,11 @@ function Portfolio() {
           Portfolio
         </div>
         <div className="projects">
-          <div className="project">
-            <img src="./site_cv_portfolio.png" alt="Site CV Hergibo Sacha" />
-            <div className="project-name">
-              Site CV
-            </div>
-            <div className="project-more">
-              <div>
-                <span><FontAwesomeIcon icon={faPlus} /></span>
-                <span>
-                  <a href="#"><FontAwesomeIcon icon={faLink} /></a>
-                </span>
-              </div>
-            </div>
-          </div>
-          <div className="project">
-          </div>
-          <div className="project">
-          </div>
-          <div className="project">
-          </div>
+          {projectPortfolio}
         </div>
+        {displayForm  && 
+          <Modal div={div} closeModal={closeModal}/>
+        }
       </div>
     </div>
   );
