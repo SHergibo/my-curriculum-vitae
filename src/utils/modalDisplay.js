62cfcfda694ebf22a5a01d6a76043910b084906f
SHergibo <sachau888@hotmail.com>
2020-10-19 15:29:38 +0200

    feat: add displayModalNoValue in utils modalDisplay.

--- src/utils/modalDisplay.js
+++ src/utils/modalDisplay.js
@@ -4,9 +4,14 @@ const displayModal = (value, setDisplayForm, setValue) => {
   setValue(value);
 }
 
+const displayModalNoValue = (setDisplayForm) => {
+  document.body.setAttribute('style', 'overflow : hidden;');
+  setDisplayForm(true);
+}
+
 const closeModal = (setDisplayForm) => {
   document.body.removeAttribute('style');
   setDisplayForm(false);
 }  
 
-export { displayModal, closeModal };
+export { displayModal, displayModalNoValue, closeModal };
