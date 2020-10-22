b401b2da149bf7f73123019730c1266676f34140
SHergibo <sachau888@hotmail.com>
2020-10-22 14:42:51 +0200

    fix: use react-scoll for logo.

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,17 +1,18 @@
 import React from "react";
-import { animateScroll as scroll } from 'react-scroll';
+import { animateScroll as scroll, Link } from 'react-scroll';
 
 function Logo() {
   const scrollToTop = () =>{
     scroll.scrollToTop();
   }
+
   return (
-    <div className="logo" onClick={scrollToTop}>
-      <a tabIndex={0}>
+    <div  className="logo" onClick={scrollToTop} onKeyPress={scrollToTop}>
+      <Link tabIndex={0} activeClass="active" to="home" spy={true} smooth={true} offset={0} duration={500}>
         <span>Sacha</span>
-      </a>
+      </Link>
     </div>
-  );
+  );    
 }
 
 export default Logo;
