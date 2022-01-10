a33b05f8044f87b2081a4c3f7a29fbb6af935c54
SHergibo <sachau888@hotmail.com>
2022-01-10 16:37:40 +0100

    feat: Show the project info directly in the component instead of using a modal in Portfolio component.

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,15 +1,13 @@
 import React, { useState, useEffect, useCallback } from "react";
 import InfoProjectModal from "./InfoProjectModal";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import Modal from "./Modal";
-import { displayModal } from './../utils/modalDisplay';
 import { apiDomain, apiVersion } from './../apiConfig/ApiConfig';
 import axios from "axios";
 import PropTypes from 'prop-types';
 
 function Portfolio({ isLoaded }) {
   const [value, setValue] = useState({});
-  const [displayForm, setDisplayForm] = useState(false);
+  const [displayProject, setDisplayProject] = useState(false);
   const [arrayProject, setArrayProject] = useState([]);
 
   const getData = useCallback(async () => {
@@ -26,6 +24,11 @@ function Portfolio({ isLoaded }) {
     }
   }, [isLoaded, getData]);
 
+  const displayProjectInfo = (item, setDisplayProject, setValue) => {
+    setDisplayProject(true);
+    setValue(item);
+  }
+
   let projectPortfolio = arrayProject.map((item) => {
     return <div className="project" key={item._id}>     
             <img src={`${apiDomain}/api/${apiVersion}/project/image/${item.img.filename}`} alt={item.altImg} />
@@ -34,7 +37,7 @@ function Portfolio({ isLoaded }) {
             </div>
             <div className="project-more">
               <div>
-                <span tabIndex={0} onClick={() => displayModal(item, setDisplayForm, setValue)} onKeyPress={() => displayModal(item, setDisplayForm, setValue)}><FontAwesomeIcon icon="plus" /></span>
+                <span tabIndex={0} onClick={() => displayProjectInfo(item, setDisplayProject, setValue)} onKeyPress={() => displayProjectInfo(item, setDisplayProject, setValue)}><FontAwesomeIcon icon="plus" /></span>
                 <span>
                   <a tabIndex={0} href={item.url}><FontAwesomeIcon icon="link" /></a>
                 </span>
@@ -49,17 +52,16 @@ function Portfolio({ isLoaded }) {
         <div className="title-right">
           Portfolio
         </div>
-        <div className="projects">
-          {projectPortfolio}
-        </div>
-        {displayForm  && 
-          <Modal 
-          div={
-            <InfoProjectModal 
-            value={value} />
-          } 
-          setDisplayForm={setDisplayForm}/>
+        {!displayProject  ?
+          <div className="projects">
+            {projectPortfolio}
+          </div> :
+          <InfoProjectModal 
+            value={value}
+            setDisplayProject={setDisplayProject} 
+          />
         }
+
       </div>
     </div>
   );
