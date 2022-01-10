48e47b224e075d4083096780fe6e166369fae16f
SHergibo <sachau888@hotmail.com>
2022-01-10 15:16:39 +0100

    fix: Added closeExteriorModal onMouseDown instead of using a useEffect to close the modal in Model component.

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,22 +1,17 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { closeModal } from './../utils/modalDisplay';
 import PropTypes from 'prop-types';
 
 function Modal({ div, setDisplayForm }) {
 
-  useEffect(() => {
-    const closeModalEventListener = window.addEventListener("click", (event) => {
-      if(event.target.className === "modal"){
+  const closeExteriorModal = (e) => {
+      if(e.target.className === "modal"){
         closeModal(setDisplayForm);
       }
-    });
-    return () => {
-      window.removeEventListener('click', closeModalEventListener);
-    }
-  }, [setDisplayForm])
+  }
 
   return (
-    <div className="modal">
+    <div className="modal" onMouseDown={(e) => closeExteriorModal(e, setDisplayForm)}>
       <div className="modal-container">
         <button className="close-modal" onClick={() => closeModal(setDisplayForm)}>X</button>
         {div}
