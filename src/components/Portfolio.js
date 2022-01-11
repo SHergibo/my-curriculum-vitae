4a7f39243cb6797389649ca9eb51674b391b7c15
SHergibo <sachau888@hotmail.com>
2022-01-11 16:33:14 +0100

    feat: Added responsive carousel and functions to use the carousel in Portfolio component.

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -9,6 +9,20 @@ function Portfolio({ isLoaded }) {
   const [value, setValue] = useState({});
   const [displayProject, setDisplayProject] = useState(false);
   const [arrayProject, setArrayProject] = useState([]);
+  const [indexProject, setIndexProject] = useState(0);
+  const [nextIndexProject, setNextIndexProject] = useState(true);
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  const responsiveWidth = useCallback(() =>{
+    setWindowWidth(window.innerWidth);
+  }, []);
+
+  useEffect(() => {
+    window.addEventListener('resize', responsiveWidth);
+    return () =>{
+      window.removeEventListener('resize', responsiveWidth);
+    }
+  }, [responsiveWidth]);
 
   const getData = useCallback(async () => {
     const getListProjectEndPoint = `${apiDomain}/api/${apiVersion}/project/project-list`;
@@ -27,15 +41,29 @@ function Portfolio({ isLoaded }) {
   const displayProjectInfo = (item, setDisplayProject, setValue) => {
     setDisplayProject(true);
     setValue(item);
+    let findIndexProject = arrayProject.findIndex(el => el._id === item._id);
+    setIndexProject(findIndexProject);
+  }
+
+  const switchProjectCarousel = (increment) => {
+    if(increment === 1){
+      setValue(arrayProject[indexProject + 1]);
+      setIndexProject(indexProject => indexProject + 1);
+      arrayProject[indexProject + 2] ? setNextIndexProject(true) : setNextIndexProject(false);
+    }else{
+      setValue(arrayProject[indexProject - 1]);
+      setIndexProject(indexProject => indexProject - 1);
+      setNextIndexProject(true);
+    }
   }
 
   let projectPortfolio = arrayProject.map((item) => {
-    return <div className="project" key={item._id}>     
+    return <div className="project-card" key={item._id}>     
             <img src={`${apiDomain}/api/${apiVersion}/project/image/${item.img.filename}`} alt={item.altImg} />
-            <div className="project-name">
+            <div className="project-card-name">
               {item.projectName}
             </div>
-            <div className="project-more">
+            <div className="project-card-more">
               <div>
                 <span tabIndex={0} onClick={() => displayProjectInfo(item, setDisplayProject, setValue)} onKeyPress={() => displayProjectInfo(item, setDisplayProject, setValue)}><FontAwesomeIcon icon="plus" /></span>
                 <span>
@@ -47,23 +75,60 @@ function Portfolio({ isLoaded }) {
   });
 
   return (
-    <div className="portfolio-container">
-      <div id="portfolio" className="wrapper portfolio">
-        <div className="title-right">
-          Portfolio
-        </div>
-        {!displayProject  ?
+    <>
+    {!displayProject &&
+      <div className="portfolio-container">
+        <div id="portfolio" className="wrapper portfolio">
+          <div className="title-right">
+            Portfolio
+          </div>
           <div className="projects">
             {projectPortfolio}
-          </div> :
-          <InfoProject 
-            value={value}
-            setDisplayProject={setDisplayProject} 
-          />
-        }
-
+          </div> 
+        </div>
       </div>
-    </div>
+    }
+
+    {displayProject &&
+      <div className="project-section">
+        <div id="portfolio" className="wrapper-project project">
+          {windowWidth >= 1087 &&
+            <div className="project-carousel-full-screen">
+              {indexProject > 0 &&
+                <button className="carousel-before" title="Projet précédant" onClick={() => {switchProjectCarousel(0)}}>
+                  <FontAwesomeIcon icon="chevron-left" />
+                </button>
+              }
+            </div>
+          }
+
+          <div>
+            <div className="title-right">
+              Projet - {value.projectName}
+            </div>
+              <InfoProject 
+                value={value}
+                setDisplayProject={setDisplayProject} 
+                windowWidth={windowWidth}
+                switchProjectCarousel={switchProjectCarousel}
+                indexProject={indexProject}
+                nextIndexProject={nextIndexProject}
+              />
+          </div>
+
+          {windowWidth >= 1087 &&
+            <div className="project-carousel-full-screen">
+            {nextIndexProject &&            
+              <button className="carousel-after" title="Projet suivant" onClick={() => {switchProjectCarousel(1)}}>
+                <FontAwesomeIcon icon="chevron-right" />
+              </button>
+            }
+            </div>
+          }
+        </div>
+      </div>
+    }
+    </>
   );
 }
 
