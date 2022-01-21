5b2a6a47a742098d63fe3f37d14e85e6ef1ded44
SHergibo <sachau888@hotmail.com>
2022-01-21 16:38:43 +0100

    feat: Used prettier in DisplayListProjects component.

--- src/components/DisplayListProjects.js
+++ src/components/DisplayListProjects.js
@@ -1,9 +1,9 @@
 import React, { useState, useEffect, useCallback } from "react";
-import axiosInstance from './../utils/axiosInstance';
-import { apiDomain, apiVersion } from '../apiConfig/ApiConfig';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import axiosInstance from "./../utils/axiosInstance";
+import { apiDomain, apiVersion } from "../apiConfig/ApiConfig";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Modal from "./Modal";
-import { displayModal } from './../utils/modalDisplay';
+import { displayModal } from "./../utils/modalDisplay";
 import FormProject from "./FormProject";
 
 function DisplayListProjects() {
@@ -13,10 +13,9 @@ function DisplayListProjects() {
 
   const getData = useCallback(async () => {
     const getListProjectPoint = `${apiDomain}/api/${apiVersion}/project/project-list`;
-    await axiosInstance.get(getListProjectPoint)
-      .then((response) => {
-        setArrayProject(response.data);
-      });
+    await axiosInstance.get(getListProjectPoint).then((response) => {
+      setArrayProject(response.data);
+    });
   }, []);
 
   useEffect(() => {
@@ -25,40 +24,57 @@ function DisplayListProjects() {
 
   const onClickDelete = async (data) => {
     const deleteProjectEndPoint = `${apiDomain}/api/${apiVersion}/project/${data._id}`;
-    await axiosInstance.delete(deleteProjectEndPoint, data)
-      .then(() => {
-        setArrayProject([...arrayProject].filter(item => item._id !== data._id));
-      });
+    await axiosInstance.delete(deleteProjectEndPoint, data).then(() => {
+      setArrayProject(
+        [...arrayProject].filter((item) => item._id !== data._id)
+      );
+    });
   };
 
   let liListProjects = arrayProject.map((item) => {
-    return <li key={item._id}>
-            <div className="div-list-container">
-              <div className="title-list">{item.projectName}</div>
-            </div>
-            <div className="div-list-btn-container">
-              <button className="btn-list-edit" title="Éditer" onClick={() => displayModal(item, setDisplayForm, setValue)}><FontAwesomeIcon icon="edit" /></button>
-              <button className="btn-list-delete" title="Supprimer" onClick={() => onClickDelete(item)}><FontAwesomeIcon icon="trash-alt" /></button>
-            </div>
-          </li>
+    return (
+      <li key={item._id}>
+        <div className="div-list-container">
+          <div className="title-list">{item.projectName}</div>
+        </div>
+        <div className="div-list-btn-container">
+          <button
+            className="btn-list-edit"
+            title="Éditer"
+            onClick={() => displayModal(item, setDisplayForm, setValue)}
+          >
+            <FontAwesomeIcon icon="edit" />
+          </button>
+          <button
+            className="btn-list-delete"
+            title="Supprimer"
+            onClick={() => onClickDelete(item)}
+          >
+            <FontAwesomeIcon icon="trash-alt" />
+          </button>
+        </div>
+      </li>
+    );
   });
 
   return (
     <>
       <div>
         <h4>Projets</h4>
-        <ul>
-        {liListProjects}
-        </ul>
+        <ul>{liListProjects}</ul>
       </div>
-      {displayForm &&
-        <Modal div={
-          <FormProject 
-          value={value}
-          projectState={{arrayProject, setArrayProject}}
-          setDisplayForm={setDisplayForm} />} 
-        setDisplayForm={setDisplayForm} />
-      }
+      {displayForm && (
+        <Modal
+          div={
+            <FormProject
+              value={value}
+              projectState={{ arrayProject, setArrayProject }}
+              setDisplayForm={setDisplayForm}
+            />
+          }
+          setDisplayForm={setDisplayForm}
+        />
+      )}
     </>
   );
 }
