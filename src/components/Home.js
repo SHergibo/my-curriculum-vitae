65d0e4699d5d5d88ca914056780642266ec39f2f
SHergibo <sachau888@hotmail.com>
2022-02-04 16:33:53 +0100

    fix: Fixed if statement when checking if generalInfo is not empty in Home component.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,53 +1,45 @@
 import React, { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import HomeSection from "./HomeSection";
-import Typed from 'react-typed';
-import PropTypes from 'prop-types';
+import Typed from "react-typed";
+import PropTypes from "prop-types";
 
 function Home({ generalInfo }) {
   const [fullName, setFullName] = useState("");
   const [welcome, setWelcome] = useState("Bienvenue");
   const [div, setDiv] = useState(
-    <div className="job-name">Je suis un <Typed
-    strings={['développeur web', 'intégrateur web']}
-    typeSpeed={80}
-    loop
-    smartBackspace={false}
-    />
+    <div className="job-name">
+      Je suis un{" "}
+      <Typed
+        strings={["développeur web", "intégrateur web"]}
+        typeSpeed={80}
+        loop
+        smartBackspace={false}
+      />
     </div>
   );
   const location = useLocation();
 
   useEffect(() => {
-    if(generalInfo){
+    if (generalInfo?.firstname) {
       setFullName(`${generalInfo.firstname} ${generalInfo.lastname}`);
-    }else{
-      setFullName('Mon site CV')
+    } else {
+      setFullName("Mon site CV");
     }
   }, [generalInfo]);
 
   useEffect(() => {
-    if(location.pathname === "/admin"){
-      setDiv(
-        <div className="sub-name">
-          Partie administration
-        </div>
-      );
-      setWelcome('Administration');
+    if (location.pathname === "/admin") {
+      setDiv(<div className="sub-name">Partie administration</div>);
+      setWelcome("Administration");
     }
-  }, [location])
+  }, [location]);
 
-  return (
-    <HomeSection 
-    welcome={welcome} 
-    name={fullName} 
-    div={div} />
-  );
+  return <HomeSection welcome={welcome} name={fullName} div={div} />;
 }
 
 Home.propTypes = {
   generalInfo: PropTypes.object,
-}
-
+};
 
 export default Home;
