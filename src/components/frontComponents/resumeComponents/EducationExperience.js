5b0bfa6599655f3aa334f1457dabe1373d07f45b
SHergibo <sachau888@hotmail.com>
2022-02-17 14:15:50 +0100

    feat: Used font-awesome icon instead of svg in EducationExperience component.

--- src/components/frontComponents/resumeComponents/EducationExperience.js
+++ src/components/frontComponents/resumeComponents/EducationExperience.js
@@ -1,46 +1,43 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import PropTypes from "prop-types";
 
-function EducationExperience({ data }) {
-
+function EducationExperience({ data, type }) {
   const formatDate = (date) => {
-    let year = date.split('-')[0];
+    let year = date.split("-")[0];
     return year;
   };
 
   const displayEducExpe = data.map((item) => {
     let formatDateStart = formatDate(item.dateStart);
     let formatDateEnd = formatDate(item.dateEnd);
-    return <div className="container-resume" key={item._id}>
-            <div className="date-resume">
-              <span className="resume-svg">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 64 64">
-                  <path data-name="layer1" fill="none" stroke="#202020" strokeMiterlimit="10" strokeWidth="2" d="M2 8h60v54H2z" strokeLinejoin="round" strokeLinecap="round">
-                  </path>
-                  <path data-name="layer2" fill="none" stroke="#202020" strokeMiterlimit="10" strokeWidth="2" d="M54 54H10V16h44v38z" strokeLinejoin="round" strokeLinecap="round">
-                  </path>
-                  <path data-name="layer1" fill="none" stroke="#202020" strokeMiterlimit="10" strokeWidth="2" d="M32 2v18M18 2v18M46 2v18" strokeLinejoin="round" strokeLinecap="round">
-                  </path>
-                </svg>
-              </span>
-              <p>{formatDateStart} - {formatDateEnd}</p>
-            </div>
-            <div className="text-resume">
-              <h3>{item.titleEducExpe}</h3>
-              <p>{item.placeEducExpe}</p>
-            </div>
-          </div>
+    return (
+      <div className="container-resume" key={item._id}>
+        <div className="date-resume">
+          <span className="resume-svg">
+            {type === "educ" ? (
+              <FontAwesomeIcon icon="graduation-cap" />
+            ) : (
+              <FontAwesomeIcon icon="briefcase" />
+            )}
+          </span>
+          <p>
+            {formatDateStart} - {formatDateEnd}
+          </p>
+        </div>
+        <div className="text-resume">
+          <h3>{item.titleEducExpe}</h3>
+          <p>{item.placeEducExpe}</p>
+        </div>
+      </div>
+    );
   });
 
-  return (
-    <>
-      {displayEducExpe}
-    </>
-  );
+  return <>{displayEducExpe}</>;
 }
 
 EducationExperience.propTypes = {
   data: PropTypes.array.isRequired,
-}
+};
 
 export default EducationExperience;
