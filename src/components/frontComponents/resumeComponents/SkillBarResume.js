4ab5a14ffa030028e320778658af07d2bb09dfa0
SHergibo <sachau888@hotmail.com>
2022-02-21 17:38:17 +0100

    fix: Fixed FontAwesome error in SkillBarResume component.

--- src/components/frontComponents/resumeComponents/SkillBarResume.js
+++ src/components/frontComponents/resumeComponents/SkillBarResume.js
@@ -1,18 +1,29 @@
 import React from "react";
 import parse from "html-react-parser";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { fab } from "@fortawesome/free-brands-svg-icons";
 import PropTypes from "prop-types";
 
 function SkillBarResume({ data }) {
   const displaySkill = data.map((item) => {
+    let fabIcon = false;
+    for (const key in fab) {
+      if (fab[key].iconName === item.fontAwesomeIcon) {
+        fabIcon = true;
+        break;
+      }
+    }
     return (
       <div className="skill-bar-container" key={item._id}>
         <div className="info-skill">
           <p>{item.nameSkill ? item.nameSkill : "default"}</p>
           {item.fontAwesomeIcon && (
             <span>
-              <FontAwesomeIcon icon={item.fontAwesomeIcon} />
-              <FontAwesomeIcon icon={["fab", item.fontAwesomeIcon]} />
+              {fabIcon ? (
+                <FontAwesomeIcon icon={["fab", item.fontAwesomeIcon]} />
+              ) : (
+                <FontAwesomeIcon icon={item.fontAwesomeIcon} />
+              )}
             </span>
           )}
           {item.svgIcon && <span>{parse(item.svgIcon)}</span>}
