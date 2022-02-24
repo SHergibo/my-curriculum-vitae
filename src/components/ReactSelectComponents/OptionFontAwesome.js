830622efde53054d62ec8f2b132be8e3a1f04867
SHergibo <sachau888@hotmail.com>
2022-02-24 16:43:58 +0100

    feat: Created OptionFontAwesome component in ReactSelectComponents folder.

---
+++ src/components/ReactSelectComponents/OptionFontAwesome.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { components } from "react-select";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+function OptionFontAwesome(props) {
+  const { Option } = components;
+  const { innerProps, isFocused, ...otherProps } = props;
+  const { onMouseMove, onMouseOver, ...otherInnerProps } = innerProps;
+  const newProps = { innerProps: { ...otherInnerProps }, ...otherProps };
+  return (
+    <Option {...newProps} className="input-select-option">
+      <FontAwesomeIcon icon={[props.data.prefix, props.data.value]} />
+      {props.data.label}
+    </Option>
+  );
+}
+
+export default OptionFontAwesome;
