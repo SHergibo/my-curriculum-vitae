ed6c3a1baaff05c77e43216da0f945998c78603b
SHergibo <sachau888@hotmail.com>
2022-02-24 16:55:51 +0100

    feat: Updated fontAwesome item data in SkillBarResume component.

--- src/components/frontComponents/resumeComponents/SkillBarResume.js
+++ src/components/frontComponents/resumeComponents/SkillBarResume.js
@@ -1,33 +1,23 @@
 import React from "react";
 import parse from "html-react-parser";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { fab } from "@fortawesome/free-brands-svg-icons";
 import PropTypes from "prop-types";
 
 function SkillBarResume({ data }) {
   const displaySkill = data.map((item) => {
-    let fabIcon = false;
-    for (const key in fab) {
-      if (fab[key].iconName === item.fontAwesomeIcon) {
-        fabIcon = true;
-        break;
-      }
-    }
     return (
       <div className="skill-bar-container" key={item._id}>
         <div className="info-skill">
           <p>{item.nameSkill ? item.nameSkill : "default"}</p>
-          {item.fontAwesomeIcon && (
+          {item.fontAwesomeIcon?.value && (
             <span>
-              {fabIcon ? (
-                <FontAwesomeIcon icon={["fab", item.fontAwesomeIcon]} />
-              ) : (
-                <FontAwesomeIcon icon={item.fontAwesomeIcon} />
-              )}
+              <FontAwesomeIcon
+                icon={[item.fontAwesomeIcon.prefix, item.fontAwesomeIcon.value]}
+              />
             </span>
           )}
           {item.svgIcon && <span>{parse(item.svgIcon)}</span>}
-          {!item.fontAwesomeIcon && !item.svgIcon && (
+          {!item.fontAwesomeIcon?.value && !item.svgIcon && (
             <span>
               <FontAwesomeIcon icon="check" />
             </span>
