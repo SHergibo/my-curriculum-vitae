db02f8a35b22a6b315a565986bcd4aa95eec4de2
SHergibo <sachau888@hotmail.com>
2022-03-09 16:41:39 +0100

    feat: Added/updated/deleted expiresIn item in localStorage in login, logout and refreshToken function in Auth.js.

--- src/utils/Auth.js
+++ src/utils/Auth.js
@@ -1,42 +1,47 @@
-import Axios from 'axios';
-import {apiDomain, apiVersion} from './../apiConfig/ApiConfig';
+import Axios from "axios";
+import { apiDomain, apiVersion } from "./../apiConfig/ApiConfig";
 
 let response;
 let authenticated = false;
 
-const loginIn = async (data) =>{
+const loginIn = async (data) => {
   const loginEndpoint = `${apiDomain}/api/${apiVersion}/auth/login`;
 
   await Axios.post(loginEndpoint, data)
-  .then((res) =>{
-    if(res.status === 200 && res.data.token.accessToken && res.data.token.expiresIn){
-      response = res.status;
-      let accessToken = res.data.token.accessToken;
-      let refresh_token = res.data.token.refreshToken.token;
-      let user_email = res.data.user.email;
-  
-      localStorage.setItem("access_token", accessToken);
-      localStorage.setItem('refresh_token', refresh_token);
-      localStorage.setItem('user_email', user_email);
-    }
-  })
-  .catch((error) =>{
-    response = 401;
-  })
+    .then((res) => {
+      if (
+        res.status === 200 &&
+        res.data.token.accessToken &&
+        res.data.token.expiresIn
+      ) {
+        response = res.status;
+
+        localStorage.setItem("access_token", res.data.token.accessToken);
+        localStorage.setItem(
+          "refresh_token",
+          res.data.token.refreshToken.token
+        );
+        localStorage.setItem("user_email", res.data.user.email);
+        localStorage.setItem("expiresIn", res.data.token.expiresIn);
+      }
+    })
+    .catch((error) => {
+      response = 401;
+    });
   return response;
 };
 
-const logout = async() =>{
+const logout = async () => {
   const logoutEndpoint = `${apiDomain}/api/${apiVersion}/auth/logout`;
   try {
     await Axios.post(logoutEndpoint, {
-      token : localStorage.getItem('refresh_token'),
-      email : localStorage.getItem('user_email')
+      token: localStorage.getItem("refresh_token"),
+      email: localStorage.getItem("user_email"),
     });
     localStorage.removeItem("access_token");
     localStorage.removeItem("refresh_token");
-    localStorage.removeItem('user_email');
-
+    localStorage.removeItem("user_email");
+    localStorage.removeItem("expiresIn");
   } catch (error) {
     console.log(error);
   }
@@ -47,62 +52,63 @@ const createAxiosHeader = () => {
     baseURL: apiDomain,
     timeout: 5000,
     headers: {
-      ContentType: 'applications/json',
-      Accept: 'application/json',
-      Authorization: `Bearer ${localStorage.getItem('access_token')}`
-    }
+      ContentType: "applications/json",
+      Accept: "application/json",
+      Authorization: `Bearer ${localStorage.getItem("access_token")}`,
+    },
   });
 };
 
-const refreshToken = async() =>{
-  const refreshToken = localStorage.getItem('refresh_token');
-  const email = localStorage.getItem('user_email');
-  if(refreshToken && email){
+const refreshToken = async () => {
+  const refreshToken = localStorage.getItem("refresh_token");
+  const email = localStorage.getItem("user_email");
+  if (refreshToken && email) {
     const refresh = createAxiosHeader();
     try {
       await refresh
-      .post(`/api/${apiVersion}/auth/refresh-token`, { 
-        refreshToken,
-        email
-      })
-      .then((response) => {
-        localStorage.setItem('access_token', response.data.accessToken);
-        localStorage.setItem('refresh_token', response.data.refreshToken.token);
-      });
+        .post(`/api/${apiVersion}/auth/refresh-token`, {
+          refreshToken,
+          email,
+        })
+        .then((response) => {
+          localStorage.setItem("access_token", response.data.accessToken);
+          localStorage.setItem(
+            "refresh_token",
+            response.data.refreshToken.token
+          );
+          localStorage.setItem("expiresIn", response.data.expiresIn);
+        });
       return true;
     } catch (error) {
       return false;
     }
-   
-  }else{
+  } else {
     return false;
   }
-
 };
 
 const checkAuth = async () => {
-  const refreshToken = localStorage.getItem('refresh_token');
-  const email = localStorage.getItem('user_email');
-  if(refreshToken && email){
+  const refreshToken = localStorage.getItem("refresh_token");
+  const email = localStorage.getItem("user_email");
+  if (refreshToken && email) {
     const checkAuth = createAxiosHeader();
     try {
-      await checkAuth
-      .get(`/api/${apiVersion}/auth/check-token`);
+      await checkAuth.get(`/api/${apiVersion}/auth/check-token`);
       return true;
     } catch (error) {
       return false;
     }
-  }else{
+  } else {
     return false;
   }
 };
 
-const isAuthenticated = async() =>{
-    if(await checkAuth()){
-      authenticated = true;
-    }else{
-      authenticated = false;
-    }
+const isAuthenticated = async () => {
+  if (await checkAuth()) {
+    authenticated = true;
+  } else {
+    authenticated = false;
+  }
 
   return authenticated;
 };
