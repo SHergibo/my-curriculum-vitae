c4b5ceb008d8462f4eba71edc87c1d8780fd3895
SHergibo <sachau888@hotmail.com>
2022-03-09 16:42:21 +0100

    fix: Added a dynamic time for refreshTokenInterval in Admin component.

--- src/components/adminComponents/Admin.js
+++ src/components/adminComponents/Admin.js
@@ -1,17 +1,22 @@
-import React, { useEffect, useState, useRef } from "react";
+import React, { useEffect, useState, useRef, useCallback } from "react";
+import { useNavigate } from "react-router-dom";
 import Home from "../frontComponents/Home";
 import Navbar from "../Navbar";
 import GeneralInfo from "./GeneralInfo";
 import Footer from "../Footer";
 import BackToTop from "../BackToTop";
-import { refreshToken } from "../../utils/Auth";
+import { refreshToken, logout } from "../../utils/Auth";
 import EducExpe from "./EducExpe";
 import Skills from "./Skills";
 import Projects from "./Projects";
 import PropTypes from "prop-types";
 
 function Admin({ generalInfoAdmin }) {
+  const navigate = useNavigate();
   const headerRef = useRef(null);
+  const [expiresIn, setExpiresIn] = useState(
+    new Date(localStorage.getItem("expiresIn"))
+  );
 
   const [generalInfo, setGeneralInfo] = useState({
     firstname: "",
@@ -36,16 +41,44 @@ function Admin({ generalInfoAdmin }) {
     }
   }, [generalInfoAdmin]);
 
+  const logOut = useCallback(async () => {
+    await logout();
+    navigate("/");
+  }, []);
+
   useEffect(() => {
     window.scrollTo(0, 0);
+    let nowDate = new Date();
+    let timeInterval = 0;
+
+    if (expiresIn.getHours() > nowDate.getHours()) {
+      timeInterval = Math.floor(
+        (expiresIn.getMinutes() - nowDate.getMinutes() + 59) * 60 * 1000
+      );
+    }
+
+    if (
+      timeInterval === 0 &&
+      expiresIn.getMinutes() - nowDate.getMinutes() - 1 <= 1
+    ) {
+      timeInterval = 1000;
+    }
+
+    if (timeInterval === 0) {
+      timeInterval = Math.floor(
+        (expiresIn.getMinutes() - nowDate.getMinutes() - 1) * 60 * 1000
+      );
+    }
+
     const refreshTokenInterval = setInterval(async () => {
       await refreshToken();
-    }, 870000);
+      setExpiresIn(new Date(localStorage.getItem("expiresIn")));
+    }, timeInterval);
 
     return () => {
       clearInterval(refreshTokenInterval);
     };
-  }, []);
+  }, [expiresIn]);
 
   return (
     <>
