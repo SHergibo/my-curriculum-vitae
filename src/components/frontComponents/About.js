5ffc8d29f9ee0ea39a9b22030192769f9d19cd39
SHergibo <sachau888@hotmail.com>
2022-03-31 16:26:40 +0200

    feat: Added AboutJobContainer component in About component.

--- src/components/frontComponents/About.js
+++ src/components/frontComponents/About.js
@@ -1,9 +1,8 @@
-import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
+import AboutJobContainer from "./AboutJobContainer";
 
 function About({ generalInfo }) {
-  
   return (
     <div id="about" className="about">
       <div className="wrapper about-container">
@@ -11,9 +10,7 @@ function About({ generalInfo }) {
           <img src="./Hergibo_Sacha.jpg" alt="Hergibo Sacha" />
         </div>
         <div className="text-about">
-          <div className="title-right">
-            À propos
-          </div>
+          <div className="title-right">À propos</div>
           <div className="about-me">
             <h2>Bonjour !</h2>
             <p>{generalInfo.description}</p>
@@ -21,44 +18,39 @@ function About({ generalInfo }) {
           <div className="info">
             <ul>
               <li>
-                <span>Téléphone :</span><div>{generalInfo.phone}</div>
+                <span>Téléphone :</span>
+                <div>{generalInfo.phone}</div>
               </li>
               <li>
-                <span>Email :</span><div>{generalInfo.email}</div>
+                <span>Email :</span>
+                <div>{generalInfo.email}</div>
               </li>
               <li>
-                <span>Adresse :</span><div>{generalInfo.address.street}, {generalInfo.address.number} {generalInfo.address.zip} {generalInfo.address.city}</div>
+                <span>Adresse :</span>
+                <div>
+                  {generalInfo.address.street}, {generalInfo.address.number}{" "}
+                  {generalInfo.address.zip} {generalInfo.address.city}
+                </div>
               </li>
               <li>
-                <span>Date de naissance :</span><div>{generalInfo.birthdate}</div>
+                <span>Date de naissance :</span>
+                <div>{generalInfo.birthdate}</div>
               </li>
               <li>
-                <span>Permis de conduire :</span><div>{generalInfo.licence}</div>
+                <span>Permis de conduire :</span>
+                <div>{generalInfo.licence}</div>
               </li>
             </ul>
           </div>
         </div>
       </div>
-      <div className="about-job-container">
-        <div className="wrapper about-job">
-          <div></div>
-          <div>
-            <FontAwesomeIcon icon={['fab', 'js']} />
-            <h5>Web Développeur</h5>
-          </div>
-          <div>
-            <FontAwesomeIcon icon={['fab', 'html5']} />
-            <h5>Web Intégrateur</h5>
-          </div>
-          <div></div>
-        </div>
-      </div>
+      <AboutJobContainer generalInfo={generalInfo} />
     </div>
   );
 }
 
 About.propTypes = {
   generalInfo: PropTypes.object.isRequired,
-}
+};
 
 export default About;
