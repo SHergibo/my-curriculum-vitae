03446ffbcad06077abd7743ff9c9e632e2666560
SHergibo <sachau888@hotmail.com>
2022-04-26 15:52:28 +0200

    feat: Added ActionInsideButtonSubmit component in EmailAuth component.

--- src/components/signInSignUpComponents/signInSignUpForms/EmailAuthForm.js
+++ src/components/signInSignUpComponents/signInSignUpForms/EmailAuthForm.js
@@ -1,15 +1,58 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 import axiosInstance from "../../../utils/axiosInstance";
 import { apiDomain, apiVersion } from "../../../apiConfig/ApiConfig";
 import { useForm } from "react-hook-form";
+import { checkSuccess, checkErrors } from "../../../utils/checkSuccess";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import ActionInsideButtonSubmit from "../../ActionInsideButtonSubmit";
 
 function EmailAuthForm({ setSwitchForm, emailFormNameState }) {
   const { emailFormName, setEmailFormName } = emailFormNameState;
 
+  const loadingRef = useRef(null);
+  const [loader, setLoader] = useState(false);
+  const successSpanRef = useRef(null);
+  const [spanSuccess, setSpanSuccess] = useState(false);
+  const errorSpanRef = useRef(null);
+  const [spanError, setSpanError] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const setTimeoutLoader = useRef();
+  const setTimeoutSuccess = useRef();
+  const setTimeoutError = useRef();
+
+  let timeoutLoader = setTimeoutLoader.current;
+  let timeoutSuccess = setTimeoutSuccess.current;
+  let timeoutError = setTimeoutError.current;
+  useEffect(() => {
+    return () => {
+      clearTimeout(timeoutLoader);
+      clearTimeout(timeoutSuccess);
+      clearTimeout(timeoutError);
+    };
+  }, [timeoutLoader, timeoutSuccess, timeoutError]);
+
   const onSubmitEmailAuth = async (data) => {
+    setLoader(true);
+    setSpanError(false);
+    setErrorMessage("");
     const sendEmailAuth = `${apiDomain}/api/${apiVersion}/email-auth`;
-    await axiosInstance.post(sendEmailAuth, data);
+    await axiosInstance
+      .post(sendEmailAuth, data)
+      .then((res) => {
+        if (res.status === 204) {
+          checkSuccess(
+            setTimeoutLoader,
+            setLoader,
+            setTimeoutSuccess,
+            setSpanSuccess
+          );
+        }
+      })
+      .catch((error) => {
+        setErrorMessage(error.response.data.output.payload.message);
+        checkErrors(setTimeoutLoader, setLoader, setTimeoutError, setSpanError);
+      });
   };
 
   const onSubmitResetPasswordMail = async (data) => {};
@@ -35,6 +78,7 @@ function EmailAuthForm({ setSwitchForm, emailFormNameState }) {
         <button
           className="returnLogin"
           title="Retour au formulaire de connexion"
+          type="button"
           onClick={() => {
             setSwitchForm(false);
             setEmailFormName("");
@@ -45,7 +89,7 @@ function EmailAuthForm({ setSwitchForm, emailFormNameState }) {
         </button>
         <h3>Renvoyer un mail de confirmation</h3>
       </div>
-      <div className="input input-login">
+      <div className="input input-signIn-signUp">
         <label htmlFor="email">Email *</label>
         <div className="input-block">
           <span>
@@ -69,11 +113,19 @@ function EmailAuthForm({ setSwitchForm, emailFormNameState }) {
           <span className="error-message-form">Ce champ est requis</span>
         )}
       </div>
-      <div className="btn-container">
-        <button className="submit-login" type="submit">
-          <span>Envoyer</span> <FontAwesomeIcon icon="envelope" />
-        </button>
-      </div>
+
+      <ActionInsideButtonSubmit
+        buttonTxt={"Envoyer"}
+        loadingRef={loadingRef}
+        loader={loader}
+        successSpanRef={successSpanRef}
+        spanSuccess={spanSuccess}
+        errorSpanRef={errorSpanRef}
+        spanError={spanError}
+      />
+      {errorMessage && !loader && (
+        <span className="error-message-signIn-signUp">{errorMessage}</span>
+      )}
     </form>
   );
 }
