e2b4c7f6863b8627eaac271033121c6df0c4e7d5
SHergibo <sachau888@hotmail.com>
2022-04-26 15:49:46 +0200

    fix: Changed setTimeoutSuccess and setTimeoutError time in checkSuccess.js.

--- src/utils/checkSuccess.js
+++ src/utils/checkSuccess.js
@@ -1,23 +1,33 @@
-const checkSuccess = async (setTimeoutLoader, setLoader, setTimeoutSuccess, setSpanSuccess) => {
-    setTimeoutLoader.current = await setTimeout(() => {
-      setLoader(false);
-      setSpanSuccess(true);
-    }, 2000);
-    setTimeoutSuccess.current = await setTimeout(() => {
-      setSpanSuccess(false);
-    }, 4000);
+const checkSuccess = async (
+  setTimeoutLoader,
+  setLoader,
+  setTimeoutSuccess,
+  setSpanSuccess
+) => {
+  setTimeoutLoader.current = await setTimeout(() => {
+    setLoader(false);
+    setSpanSuccess(true);
+  }, 2000);
+  setTimeoutSuccess.current = await setTimeout(() => {
+    setSpanSuccess(false);
+  }, 6000);
   return;
-}
+};
 
-const checkErrors = async (setTimeoutLoader, setLoader, setTimeoutError, setSpanError) => {
-    setTimeoutLoader.current = await setTimeout(() => {
-      setLoader(false);
-      setSpanError(true);
-    }, 2000);
-    setTimeoutError.current = await setTimeout(() => {
-      setSpanError(false);
-    }, 6000);
+const checkErrors = async (
+  setTimeoutLoader,
+  setLoader,
+  setTimeoutError,
+  setSpanError
+) => {
+  setTimeoutLoader.current = await setTimeout(() => {
+    setLoader(false);
+    setSpanError(true);
+  }, 2000);
+  setTimeoutError.current = await setTimeout(() => {
+    setSpanError(false);
+  }, 6000);
   return;
-}
+};
 
-export {checkSuccess, checkErrors};
+export { checkSuccess, checkErrors };
