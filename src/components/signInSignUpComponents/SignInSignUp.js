ced1ee8d029a00a7adfe8ee341dabb06cb2c1250
SHergibo <sachau888@hotmail.com>
2022-04-27 16:16:21 +0200

    fix: Deleted ternary if to switch between components in SignInSignUp component.

--- src/components/signInSignUpComponents/SignInSignUp.js
+++ src/components/signInSignUpComponents/SignInSignUp.js
@@ -1,31 +1,10 @@
-import React, { useState } from "react";
+import React from "react";
 import HomeSection from "../frontComponents/HomeSection";
 import LoginForm from "./signInSignUpForms/LoginForm";
-import EmailAuthForm from "./signInSignUpForms/EmailAuthForm";
 
 function SignInSignUp() {
-  const [switchForm, setSwitchForm] = useState(false);
-  const [emailFormName, setEmailFormName] = useState("");
-
   return (
-    <HomeSection
-      welcome="Connexion"
-      name="Bienvenue"
-      div={
-        !switchForm ? (
-          <LoginForm
-            setSwitchForm={setSwitchForm}
-            setEmailFormName={setEmailFormName}
-            emailFormNameState={{ emailFormName, setEmailFormName }}
-          />
-        ) : (
-          <EmailAuthForm
-            setSwitchForm={setSwitchForm}
-            emailFormNameState={{ emailFormName, setEmailFormName }}
-          />
-        )
-      }
-    />
+    <HomeSection welcome="Connexion" name="Bienvenue" div={<LoginForm />} />
   );
 }
 
