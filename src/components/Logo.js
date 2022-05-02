bfc8f3fd9f5847e6097b617159e1b30ceeb2c023
SHergibo <sachau888@hotmail.com>
2022-05-02 14:36:55 +0200

    feat: Used useInfosData context to use firstname from user infos instead of a hardcoded name in Logo component.

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,18 +1,28 @@
 import React from "react";
-import { animateScroll as scroll, Link } from 'react-scroll';
+import { useInfosData } from "../App";
+import { animateScroll as scroll, Link } from "react-scroll";
 
 function Logo() {
-  const scrollToTop = () =>{
+  const { generalInfo } = useInfosData();
+  const scrollToTop = () => {
     scroll.scrollToTop();
-  }
+  };
 
   return (
-    <div  className="logo" onClick={scrollToTop} onKeyPress={scrollToTop}>
-      <Link tabIndex={0} activeClass="active" to="home" spy={true} smooth={true} offset={0} duration={500}>
-        <span>Sacha</span>
+    <div className="logo" onClick={scrollToTop} onKeyPress={scrollToTop}>
+      <Link
+        tabIndex={0}
+        activeClass="active"
+        to="home"
+        spy={true}
+        smooth={true}
+        offset={0}
+        duration={500}
+      >
+        <span>{generalInfo.firstname}</span>
       </Link>
     </div>
-  );    
+  );
 }
 
 export default Logo;
