8fbc8cc3222d9eb8ff8b7366f7e41d6e9e88b346
SHergibo <sachau888@hotmail.com>
2022-05-13 14:49:50 +0200

    feat: Added logic to hide or show AboutJobContainer component when there is datas to show in mobile or tablet.

--- src/components/frontComponents/AboutJobContainer.js
+++ src/components/frontComponents/AboutJobContainer.js
@@ -1,154 +1,173 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import parse from "html-react-parser";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import PropTypes from "prop-types";
 
 function AboutJobContainer({ generalInfo }) {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  const handleResize = () => {
+    setWindowWidth(window.innerWidth);
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
-    <div className="about-job-container">
-      {generalInfo.professionTitles.length === 0 && (
-        <div className="wrapper about-job-four">
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
-        </div>
-      )}
-      {generalInfo.professionTitles.length === 1 && (
-        <div className="wrapper about-job-one">
-          <div></div>
-          <div>
-            {generalInfo.professionTitles[0].fontAwesomeIcon?.value && (
-              <FontAwesomeIcon
-                icon={[
-                  generalInfo.professionTitles[0].fontAwesomeIcon.prefix,
-                  generalInfo.professionTitles[0].fontAwesomeIcon.value,
-                ]}
-              />
-            )}
-            {generalInfo.professionTitles[0].svgIconProfTitle && (
-              <span>
-                {parse(generalInfo.professionTitles[0].svgIconProfTitle)}
-              </span>
-            )}
-            <p>{generalInfo.professionTitles[0].nameProfessionTitle}</p>
-          </div>
-          <div></div>
-        </div>
-      )}
-      {generalInfo.professionTitles.length === 2 && (
-        <div className="wrapper about-job-two">
-          <div></div>
-          <div>
-            {generalInfo.professionTitles[0].fontAwesomeIcon?.value && (
-              <FontAwesomeIcon
-                icon={[
-                  generalInfo.professionTitles[0].fontAwesomeIcon.prefix,
-                  generalInfo.professionTitles[0].fontAwesomeIcon.value,
-                ]}
-              />
-            )}
-            {generalInfo.professionTitles[0].svgIconProfTitle && (
-              <span>
-                {parse(generalInfo.professionTitles[0].svgIconProfTitle)}
-              </span>
-            )}
-            <p>{generalInfo.professionTitles[0].nameProfessionTitle}</p>
-          </div>
-          <div>
-            {generalInfo.professionTitles[1].fontAwesomeIcon?.value && (
-              <FontAwesomeIcon
-                icon={[
-                  generalInfo.professionTitles[1].fontAwesomeIcon.prefix,
-                  generalInfo.professionTitles[1].fontAwesomeIcon.value,
-                ]}
-              />
-            )}
-            {generalInfo.professionTitles[1].svgIconProfTitle && (
-              <span>
-                {parse(generalInfo.professionTitles[1].svgIconProfTitle)}
-              </span>
-            )}
-            <p>{generalInfo.professionTitles[1].nameProfessionTitle}</p>
-          </div>
-          <div></div>
-        </div>
-      )}
-      {generalInfo.professionTitles.length === 3 && (
-        <div className="wrapper about-job-three">
-          <div>
-            {generalInfo.professionTitles[0].fontAwesomeIcon?.value && (
-              <FontAwesomeIcon
-                icon={[
-                  generalInfo.professionTitles[0].fontAwesomeIcon.prefix,
-                  generalInfo.professionTitles[0].fontAwesomeIcon.value,
-                ]}
-              />
-            )}
-            {generalInfo.professionTitles[0].svgIconProfTitle && (
-              <span>
-                {parse(generalInfo.professionTitles[0].svgIconProfTitle)}
-              </span>
-            )}
-            <p>{generalInfo.professionTitles[0].nameProfessionTitle}</p>
-          </div>
-          <div>
-            {generalInfo.professionTitles[1].fontAwesomeIcon?.value && (
-              <FontAwesomeIcon
-                icon={[
-                  generalInfo.professionTitles[1].fontAwesomeIcon.prefix,
-                  generalInfo.professionTitles[1].fontAwesomeIcon.value,
-                ]}
-              />
-            )}
-            {generalInfo.professionTitles[1].svgIconProfTitle && (
-              <span>
-                {parse(generalInfo.professionTitles[1].svgIconProfTitle)}
-              </span>
-            )}
-            <p>{generalInfo.professionTitles[1].nameProfessionTitle}</p>
-          </div>
-          <div>
-            {generalInfo.professionTitles[2].fontAwesomeIcon?.value && (
-              <FontAwesomeIcon
-                icon={[
-                  generalInfo.professionTitles[2].fontAwesomeIcon.prefix,
-                  generalInfo.professionTitles[2].fontAwesomeIcon.value,
-                ]}
-              />
-            )}
-            {generalInfo.professionTitles[2].svgIconProfTitle && (
-              <span>
-                {parse(generalInfo.professionTitles[2].svgIconProfTitle)}
-              </span>
-            )}
-            <p>{generalInfo.professionTitles[2].nameProfessionTitle}</p>
-          </div>
-        </div>
-      )}
-      {generalInfo.professionTitles.length === 4 && (
-        <div className="wrapper about-job-four">
-          {generalInfo.professionTitles.map((profTitle) => {
-            return (
-              <div key={profTitle.id}>
-                {profTitle.fontAwesomeIcon?.value && (
+    <>
+      {(windowWidth > 960 ||
+        (windowWidth < 960 && generalInfo.professionTitles.length >= 1)) && (
+        <div className="about-job-container">
+          {generalInfo.professionTitles.length === 0 && (
+            <div className="wrapper about-job-four">
+              <div></div>
+              <div></div>
+              <div></div>
+              <div></div>
+            </div>
+          )}
+
+          {generalInfo.professionTitles.length === 1 && (
+            <div className="wrapper about-job-one">
+              <div></div>
+              <div>
+                {generalInfo.professionTitles[0].fontAwesomeIcon?.value && (
                   <FontAwesomeIcon
                     icon={[
-                      profTitle.fontAwesomeIcon.prefix,
-                      profTitle.fontAwesomeIcon.value,
+                      generalInfo.professionTitles[0].fontAwesomeIcon.prefix,
+                      generalInfo.professionTitles[0].fontAwesomeIcon.value,
                     ]}
                   />
                 )}
-                {profTitle.svgIconProfTitle && (
-                  <span>{parse(profTitle.svgIconProfTitle)}</span>
+                {generalInfo.professionTitles[0].svgIconProfTitle && (
+                  <span>
+                    {parse(generalInfo.professionTitles[0].svgIconProfTitle)}
+                  </span>
                 )}
-                <p>{profTitle.nameProfessionTitle}</p>
+                <p>{generalInfo.professionTitles[0].nameProfessionTitle}</p>
               </div>
-            );
-          })}
+              <div></div>
+            </div>
+          )}
+          {generalInfo.professionTitles.length === 2 && (
+            <div className="wrapper about-job-two">
+              <div></div>
+              <div>
+                {generalInfo.professionTitles[0].fontAwesomeIcon?.value && (
+                  <FontAwesomeIcon
+                    icon={[
+                      generalInfo.professionTitles[0].fontAwesomeIcon.prefix,
+                      generalInfo.professionTitles[0].fontAwesomeIcon.value,
+                    ]}
+                  />
+                )}
+                {generalInfo.professionTitles[0].svgIconProfTitle && (
+                  <span>
+                    {parse(generalInfo.professionTitles[0].svgIconProfTitle)}
+                  </span>
+                )}
+                <p>{generalInfo.professionTitles[0].nameProfessionTitle}</p>
+              </div>
+              <div>
+                {generalInfo.professionTitles[1].fontAwesomeIcon?.value && (
+                  <FontAwesomeIcon
+                    icon={[
+                      generalInfo.professionTitles[1].fontAwesomeIcon.prefix,
+                      generalInfo.professionTitles[1].fontAwesomeIcon.value,
+                    ]}
+                  />
+                )}
+                {generalInfo.professionTitles[1].svgIconProfTitle && (
+                  <span>
+                    {parse(generalInfo.professionTitles[1].svgIconProfTitle)}
+                  </span>
+                )}
+                <p>{generalInfo.professionTitles[1].nameProfessionTitle}</p>
+              </div>
+              <div></div>
+            </div>
+          )}
+          {generalInfo.professionTitles.length === 3 && (
+            <div className="wrapper about-job-three">
+              <div>
+                {generalInfo.professionTitles[0].fontAwesomeIcon?.value && (
+                  <FontAwesomeIcon
+                    icon={[
+                      generalInfo.professionTitles[0].fontAwesomeIcon.prefix,
+                      generalInfo.professionTitles[0].fontAwesomeIcon.value,
+                    ]}
+                  />
+                )}
+                {generalInfo.professionTitles[0].svgIconProfTitle && (
+                  <span>
+                    {parse(generalInfo.professionTitles[0].svgIconProfTitle)}
+                  </span>
+                )}
+                <p>{generalInfo.professionTitles[0].nameProfessionTitle}</p>
+              </div>
+              <div>
+                {generalInfo.professionTitles[1].fontAwesomeIcon?.value && (
+                  <FontAwesomeIcon
+                    icon={[
+                      generalInfo.professionTitles[1].fontAwesomeIcon.prefix,
+                      generalInfo.professionTitles[1].fontAwesomeIcon.value,
+                    ]}
+                  />
+                )}
+                {generalInfo.professionTitles[1].svgIconProfTitle && (
+                  <span>
+                    {parse(generalInfo.professionTitles[1].svgIconProfTitle)}
+                  </span>
+                )}
+                <p>{generalInfo.professionTitles[1].nameProfessionTitle}</p>
+              </div>
+              <div>
+                {generalInfo.professionTitles[2].fontAwesomeIcon?.value && (
+                  <FontAwesomeIcon
+                    icon={[
+                      generalInfo.professionTitles[2].fontAwesomeIcon.prefix,
+                      generalInfo.professionTitles[2].fontAwesomeIcon.value,
+                    ]}
+                  />
+                )}
+                {generalInfo.professionTitles[2].svgIconProfTitle && (
+                  <span>
+                    {parse(generalInfo.professionTitles[2].svgIconProfTitle)}
+                  </span>
+                )}
+                <p>{generalInfo.professionTitles[2].nameProfessionTitle}</p>
+              </div>
+            </div>
+          )}
+          {generalInfo.professionTitles.length === 4 && (
+            <div className="wrapper about-job-four">
+              {generalInfo.professionTitles.map((profTitle) => {
+                return (
+                  <div key={profTitle.id}>
+                    {profTitle.fontAwesomeIcon?.value && (
+                      <FontAwesomeIcon
+                        icon={[
+                          profTitle.fontAwesomeIcon.prefix,
+                          profTitle.fontAwesomeIcon.value,
+                        ]}
+                      />
+                    )}
+                    {profTitle.svgIconProfTitle && (
+                      <span>{parse(profTitle.svgIconProfTitle)}</span>
+                    )}
+                    <p>{profTitle.nameProfessionTitle}</p>
+                  </div>
+                );
+              })}
+            </div>
+          )}
         </div>
       )}
-    </div>
+    </>
   );
 }
 
